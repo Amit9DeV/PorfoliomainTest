5925365867e7e70bedf8738b845d2484169d82ee
Amit Kumar Ram <119648331+Amit9DeV@users.noreply.github.com>
2025-01-26 00:05:34 +0530

    footer updated for mobile view

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FaReact } from "react-icons/fa";
 export default function Footer() {
   return (
     <>
-    <footer className=" absolute bottom-0 w-full flex justify-between px-8 border backdrop-blur-[2px] ">
+    <footer className=" text-xs md:text-lg absolute bottom-0 w-full flex justify-between px-8 border backdrop-blur-[2px] ">
       <div>Master</div>
       <div className="flex">
         <li className="flex list-none px-2 items-center justify-center" >Ln 9,Col 51</li>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import {
   Drawer,
   DrawerClose,
@@ -9,41 +9,51 @@ import {
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
-} from "@/components/ui/drawer"
-
+} from "@/components/ui/drawer";
 
 const navigation = [
-  { name: 'Home', href: '/' },
-  { name: 'About', href: '/About' },
-  { name: 'Projects', href: '/Projects' },
-  { name: 'Contact', href: '/Contact' },
-]
+  { name: "Home", href: "/" },
+  { name: "About", href: "/About" },
+  { name: "Projects", href: "/Projects" },
+  { name: "Contact", href: "/Contact" },
+];
+
+const [active,setActive] = useState(true);
 
 export default function NavBar() {
-
   return (
-<>
+    <>
+   
       <header className="absolute inset-x-0 top-0 z-50 flex justify-center  ">
-      <Drawer>
-      
-      <div className='border w-full h-[1px] absolute top-9'></div>
-      <DrawerTrigger className="backdrop-blur-3xl text-red-500 border p-2 font-bold mt-5 ">Do Not Click</DrawerTrigger>
-      <DrawerContent className="flex items-center justify-between p-3 px-9 md:p-7 mt-1 border backdrop-blur-2xl  ">
-        <nav aria-label="Global" className="flex items-center justify-between p-3 px-9 md:p-7 mt-1 border backdrop-blur-2xl  " >
-          <div className="flex gap-3 items-center justify-center md:gap-x-12">
-            {navigation.map((item) => (
-              <NavLink  key={item.name} to={item.href} className="text-lg font-semibold leading-6 border-b-2 hover:border-b-pink-600 text-white hover:text-yellow-600 ">
-                {item.name}
-              </NavLink>
-            ))}
-          </div>
-
-        </nav>
-        </DrawerContent>
+        <Drawer>
+          <div className="border w-full h-[1px] absolute top-9"></div>
+          <DrawerTrigger className="backdrop-blur-3xl text-red-500 border p-2 font-bold mt-5 ">
+            Do Not Click
+          </DrawerTrigger>
+          <DrawerContent>
+          { active && 
+            <nav
+              aria-label="Global"
+              className="flex items-center justify-between p-3 px-9 md:p-7 mt-1 border backdrop-blur-2xl  "
+            >
+              <div className="flex gap-3 items-center justify-center md:gap-x-12">
+                {navigation.map((item) => (
+                  <NavLink
+                    onClick={()=>setActive(!active)}
+                    key={item.name}
+                    to={item.href}
+                    className="text-lg font-semibold leading-6 border-b-2 hover:border-b-pink-600 text-white hover:text-yellow-600 "
+                  >
+                    {item.name}
+                  </NavLink>
+                ))}
+              </div>
+            </nav>
+          }
+          </DrawerContent>
         </Drawer>
       </header>
-
-    
-</>
-  )
+      
+    </>
+  );
 }
